476c7644a9257a286044c797f97fb7f70160f7b3
Popkov Oleg <popkovoa@ektaservice.ua>
2022-10-18 19:35:09 +0300

    initial commit

---
+++ internal/user/handler.go
@@ -0,0 +1,62 @@
+package user
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"restApi/internal/handlers"
+	"restApi/pkg/logging"
+)
+
+const (
+	usersUrl = "/users"
+	userUrl  = "/users/:id"
+)
+
+type handler struct {
+	logger logging.Logger
+}
+
+func NewHandler(logger logging.Logger) handlers.Handler {
+	return &handler{
+		logger: logger,
+	}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersUrl, h.GetList)
+	router.POST(userUrl, h.CreateUser)
+	router.GET(userUrl, h.GetUserById)
+	router.PUT(userUrl, h.UpdateUser)
+	router.PATCH(userUrl, h.PartiallyUpdateUser)
+	router.DELETE(userUrl, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is list of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("this create user"))
+}
+
+func (h *handler) GetUserById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("this is user by id"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this update user"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("this is delete user"))
+}
